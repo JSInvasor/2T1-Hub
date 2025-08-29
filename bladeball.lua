0d078941e933eb3c75c236dd8ba9f17c0eb85934
Beyefendi <85225330+JSInvasor@users.noreply.github.com>
2025-08-29 17:49:19 +0300

    Update bladeball.lua

--- bladeball.lua
+++ bladeball.lua
@@ -1,6 +1,5 @@
 local Material = loadstring(game:HttpGet("https://raw.githubusercontent.com/Kinlei/MaterialLua/master/Module.lua"))()
 
--- Sol üst köşeye logo/decal ekleme
 local function AddLogo(imageId)
     local ScreenGui = Instance.new("ScreenGui")
     ScreenGui.Parent = game.Players.LocalPlayer:WaitForChild("PlayerGui")
@@ -11,12 +10,11 @@ local function AddLogo(imageId)
     ImageLabel.BackgroundTransparency = 1
     ImageLabel.Position = UDim2.new(0, 10, 0, 10)
     ImageLabel.Size = UDim2.new(0, 100, 0, 100)
-    ImageLabel.Image = "rbxassetid://" .. imageId -- Decal ID buraya gelecek
+    ImageLabel.Image = "rbxassetid://" .. imageId
     ImageLabel.ScaleType = Enum.ScaleType.Fit
 end
 
--- Decal ID'yi buraya gir (örnek: 1234567890)
-AddLogo("125037834893944") -- BURAYA DECAL ID'NİZİ GİRİN
+AddLogo("125037834893944")
 
 local X = Material.Load({
     Title = " 2t1 Hub * Private",
@@ -45,100 +43,6 @@ local B = Y.Toggle({
     Enabled = false
 })
 
-local C = Y.Slider({
-    Text = "Slip and... you get the idea",
-    Callback = function(Value)
-        print(Value)
-    end,
-    Min = 200,
-    Max = 400,
-    Def = 300
-})
-
-local D = Y.Dropdown({
-    Text = "Dropping care package",
-    Callback = function(Value)
-        print(Value)
-    end,
-    Options = {
-        "Floor 1",
-        "Floor 2",
-        "Floor 3",
-        "Floor 4",
-        "Floor 5"
-    },
-    Menu = {
-        Information = function(self)
-            X.Banner({
-                Text = "Test alert!"
-            })
-        end
-    }
-})
-
-local E = Y.ChipSet({
-    Text = "Chipping away",
-    Callback = function(ChipSet)
-        table.foreach(ChipSet, function(Option, Value)
-            print(Option, Value)
-        end)
-    end,
-    Options = {
-        TeamCheck = false,
-        UselessBool = {
-            Enabled = true,
-            Menu = {
-                Information = function(self)
-                    X.Banner({
-                        Text = "This bool has absolutely no purpose whatsoever."
-                    })
-                end
-            }
-        }
-    }
-})
-
-local F = Y.DataTable({
-    Text = "Chipping away",
-    Callback = function(ChipSet)
-        table.foreach(ChipSet, function(Option, Value)
-            print(Option, Value)
-        end)
-    end,
-    Options = {
-        TeamCheck2 = false,
-        UselessBool2 = {
-            Enabled = true,
-            Menu = {
-                Information = function(self)
-                    X.Banner({
-                        Text = "This bool ALSO has absolutely no purpose. Sorry."
-                    })
-                end
-            }
-        }
-    }
-})
-
-local H = Y.TextField({
-    Text = "Country",
-    Callback = function(Value)
-        print(Value)
-    end,
-    Menu = {
-        GB = function(self)
-            self.SetText("GB")
-        end,
-        JP = function(self)
-            self.SetText("JP")
-        end,
-        KO = function(self)
-            self.SetText("KO")
-        end
-    }
-})
-
--- RAGE SEKMESINE RAGE PARRY BUTONU
 local RageParryButton = Z.Toggle({
     Text = "Rage Parry",
     Callback = function(Value)
